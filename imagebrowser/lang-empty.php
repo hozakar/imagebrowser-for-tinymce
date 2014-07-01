f5ff8e289a73fecd86f066a3c59e54db99f5195d
bbelt <hozakar@gmail.com>
2014-07-01 23:05:56 +0300

    1.0.0.01
    ImageBrowser

---
+++ imagebrowser/lang-empty.php
@@ -0,0 +1,22 @@
+<?php
+    $translate = array(
+        "Directory could not be deleted!"                   => "",
+        "Image"                                             => "",
+        "Float"                                             => "",
+        "Width"                                             => "",
+        "Free"                                              => "",
+        "None"                                              => "",
+        "Left"                                              => "",
+        "Right"                                             => "",
+        "UPLOAD"                                            => "",
+        "DELETE"                                            => "",
+        "CUT"                                               => "",
+        "COPY"                                              => "",
+        "PASTE"                                             => "",
+        "Add Directory"                                     => "",
+        "Delete"                                            => "",
+        "Are You Sure You Want To Delete This Directory?"   => "",
+        "Are you sure you want to delete this file?"        => "",
+        "Are you sure you want to delete selected file(s)?" => ""
+    );
+?>
---
+++ imagebrowser/lang-tr_TR.php
@@ -0,0 +1,22 @@
+<?php
+    $translate = array(
+        "Directory could not be deleted!"                   => "Dizin Silinemedi!",
+        "Image"                                             => "İmaj",
+        "Float"                                             => "Konum",
+        "Width"                                             => "Genişlik",
+        "Free"                                              => "Serbest",
+        "None"                                              => "Yok",
+        "Left"                                              => "Sol",
+        "Right"                                             => "Sağ",
+        "UPLOAD"                                            => "YÜKLE",
+        "DELETE"                                            => "SİL",
+        "CUT"                                               => "KES",
+        "COPY"                                              => "KOPYALA",
+        "PASTE"                                             => "YAPIŞTIR",
+        "Add Directory"                                     => "Dizin Ekle",
+        "Delete"                                            => "Sil",
+        "Are You Sure You Want To Delete This Directory?"   => "Bu Dizini Silmek İstediğinize Emin misiniz?",
+        "Are You Sure You Want To Delete This File?"        => "Bu Dosyayı Silmek İstediğinize Emin misiniz",
+        "Are You Sure You Want To Delete Selected File(s)?" => "Seçili Dosya(lar)ı Silmek İstediğinize Emin misiniz?"
+    );
+?>
